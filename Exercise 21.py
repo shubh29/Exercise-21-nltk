ba470c7b762963725f7a64bf5f9b66af4a1bf625
Shubham Mahajan <shubham.mahajan@icloud.com>
2015-10-07 16:49:38 -0400

    after adding functions writing to text and getting bigrams

--- Exercise 21.py
+++ Exercise 21.py
@@ -12,7 +12,7 @@ from nltk.tokenize import RegexpTokenizer
 from urllib import request
 from bs4 import BeautifulSoup
 
-def write(url):
+def fetchContents(url):
     fail = False
     print( "Attempting to open ", url)
     try:
@@ -26,5 +26,33 @@ def write(url):
     tokens = tokenizer.tokenize(nohtml)
     print(tokens)
 
+fetchContents("http://money.cnn.com/data/dow30/")
 
-write("http://money.cnn.com/data/dow30/")
+def writeToTextFile():
+    print('Creating a new text file\n')
+    #Name of text file coerced with .txt
+    textFileName = input('Enter name of text file: ') + '.txt'
+    
+    try:
+        print('\nWriting to the text file..\n')
+        #Trying to create a new file or open one
+        writeToTextFile = open(textFileName, "w")
+        writeToTextFile.write(str(tokens))
+        writeToTextFile.close()
+        print('File writing completed!\n')
+    except:
+        print('Something went wrong while writing to the file!')
+        #quit python
+        sys.exit(0)
+writeToTextFile()
+
+def generatebigrams():
+    pairs = nltk.bigrams(tokens)
+    bigram_measures = nltk.collocations.BigramAssocMeasures()
+    finder = nltk.collocations.BigramCollocationFinder.from_words(pairs)
+    finder.apply_freq_filter(3)
+    forprint = finder.nbest(bigram_measures.pmi, 10)
+    print(forprint)
+generatebigrams()
+
+
