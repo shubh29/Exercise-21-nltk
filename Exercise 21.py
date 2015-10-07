7b314f08a1c5326e308f41f696b657265a56332c
Shubham Mahajan <shubham.mahajan@icloud.com>
2015-10-07 16:08:17 -0400

    created a method and passed url as a argument

--- Exercise 21.py
+++ Exercise 21.py
@@ -7,19 +7,24 @@ Created on Wed Oct  7 15:45:30 2015
 
 import nltk
 import urllib
+from nltk import *
+from nltk.tokenize import RegexpTokenizer
 from urllib import request
 from bs4 import BeautifulSoup
 
-fail = False
-url = "http://money.cnn.com/data/dow30/"
-print( "Attempting to open ", url)
-try:
-   linecount=0
-   page=request.urlopen(url)
-except:
-   print("\nCould not open URL: ", url)
-   fail = True
+def write(url):
+    fail = False
+    print( "Attempting to open ", url)
+    try:
+        linecount=0
+        page=request.urlopen(url)
+    except:
+        print("\nCould not open URL: ", url)
+        fail = True
+    nohtml = BeautifulSoup(page).get_text()
+    tokenizer = RegexpTokenizer(r'\w+')
+    tokens = tokenizer.tokenize(nohtml)
+    print(tokens)
 
-nohtml = BeautifulSoup(page).get_text()
-tokens = nltk.word_tokenize(nohtml)
-print(tokens)
+
+write("http://money.cnn.com/data/dow30/")
